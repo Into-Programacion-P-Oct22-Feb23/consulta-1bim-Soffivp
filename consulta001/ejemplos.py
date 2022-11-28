c4554b6c4f00465d40fbecce8cec7d0df57f60b8
Soffi <psvire@utpl.edu.ec>
2022-11-27 19:14:12 -0500

    Ejemplos python

--- consulta001/ejemplos.py
+++ consulta001/ejemplos.py
@@ -1,11 +1,12 @@
 class my_class(object):
+    #ejemplo1
     nombre = input("Ingrese su nombre ")
     print("Su nombre es:", nombre)
-
+    #ejemplo2
     edad = input("Ingrese su edad ")
     edad = int(edad)
     print("En 12 meses trendras:", edad+1)
-
+    #ejemplo3
     estatura = input("Ingrese su estatura ")
     estatura = float(estatura)
     print("Su estatura es:", estatura)
